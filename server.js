1378c5626f3b2abd4f2640ae95e3e2d2f256c8b5
silvonei <silvoneildr@gmail.com>
2020-01-14 17:00:01 -0300

    Updates

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
-
+const winston = require("winston");
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+
+const port = 3035;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
@@ -17,14 +19,24 @@ app.use('/', (req, res) => {
 let messages = [];
 
 io.on('connection', socket => {
-  console.log(`Socket conectado ${socket.id}`);
+  // console.log(`Socket conectado ${socket.id}`);
 
-  socket.emit('previousMessage', messages);
+  // socket.emit('previousMessage', messages);
 
-  socket.on('sendMessage', data => {
-    messages.push(data);
-    socket.broadcast.emit('receivedMessage', data);
-  });
+  // socket.on('sendMessage', data => {
+  //   messages.push(data);
+  //   socket.broadcast.emit('receivedMessage', data);
+  // });
+  socket.on('broadcast-notification-monitor-alert-solved', (data) => {
+    console.log(data);
+    io.sockets.emit('new-alert-solved-notification', {
+      ...data,
+      id: socket.id,
+      hasChanges: true,
+    });
+  })
 });
 
-server.listen(3035);
+server.listen(port);
+
+winston.log("info", "Server listening on localhost: " + port);
